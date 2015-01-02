369958163cb370098e301c5152db8b810eb1fe56
ipobornik <gereg.sith@gmail.com>
2015-01-02 22:44:15 +0200

    add ng-view

--- ads-app/app/js/app.js
+++ ads-app/app/js/app.js
@@ -1,1 +1,14 @@
-var adsApp = angular.module("adsApp", ["ngResource"]);
+var adsApp = angular
+    .module("adsApp", ["ngResource", "ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/edit-ad", {
+                templateUrl: "app/templates/edit-ad.html"
+            })
+            .when("/details", {
+            	 templateUrl: "app/templates/details.html"
+            })
+            .otherwise({
+            	redirectTo: "/home"
+            });
+    });
