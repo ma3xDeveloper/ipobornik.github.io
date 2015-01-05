f36a4e655549dbcd2e1708501fcb95ae2740a297
ipobornik <gereg.sith@gmail.com>
2015-01-05 20:33:18 +0200

    css

--- ads-app/app/js/controllers/AdDetailsController.js
+++ ads-app/app/js/controllers/AdDetailsController.js
@@ -1,12 +1,12 @@
 adsApp.controller("AdDetailsController",
     function AdDetailsController($scope, adsData) {
 
-        $scope.ad = adsData.getAd("");
+        $scope.ad = adsData.getAd("?pagesize=1");
 
         for (var p in $scope.ad) {
             console.log(p);
         }
 
-        console.log( $scope.ad.$get);
+        console.log( $scope.ad);
 
     });
